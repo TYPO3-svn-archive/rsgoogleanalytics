3d442b0253ba45decdd8c0a0e775e9065b405896
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-19 10:32:48 +0000

    Move static TS to a sub-folder, references #30023
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rsgoogleanalytics/branches/version2-dev@52224 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
-// Define the path to the static TS files
-
-t3lib_extMgm::addStaticFile($_EXTKEY, 'static/', 'Google Analytics');
+	// Define the path to the static TS files
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/general/', 'Google Analytics (General Setup)');
 ?>
