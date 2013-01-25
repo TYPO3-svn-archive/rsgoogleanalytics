3fdbaee808b8154e7fd1e97514043835418627c3
nxpthx <nxpthx@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-25 11:14:46 +0000

    [TASK] Release of Version 3.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rsgoogleanalytics/trunk@70305 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "rsgoogleanalytics".
 #
-# Auto generated 24-06-2012 12:38
+# Auto generated 25-01-2013 12:13
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Supports full options of Google Analytics with new ga.js (within Domains, FilesLinks, External URLs e.g.)',
 	'category' => 'RS WebSystems',
 	'shy' => 0,
-	'version' => '3.0.0',
+	'version' => '3.1.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"564c";s:30:"class.tx_rsgoogleanalytics.php";s:4:"a675";s:12:"ext_icon.gif";s:4:"18fd";s:17:"ext_localconf.php";s:4:"0064";s:14:"ext_tables.php";s:4:"0d4f";s:13:"locallang.xml";s:4:"9db3";s:14:"doc/manual.pdf";s:4:"1b06";s:14:"doc/manual.sxw";s:4:"3b26";s:33:"res/templates/codeAsynchronous.js";s:4:"e6eb";s:32:"res/templates/codeTraditional.js";s:4:"127a";s:16:"static/setup.txt";s:4:"5a95";s:29:"static/asynchronous/setup.txt";s:4:"e921";s:28:"static/general/constants.txt";s:4:"d62e";s:24:"static/general/setup.txt";s:4:"a3aa";s:28:"static/traditional/setup.txt";s:4:"f2dd";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"974f";s:30:"class.tx_rsgoogleanalytics.php";s:4:"ec76";s:12:"ext_icon.gif";s:4:"18fd";s:17:"ext_localconf.php";s:4:"0064";s:14:"ext_tables.php";s:4:"0d4f";s:13:"locallang.xml";s:4:"9db3";s:14:"doc/manual.pdf";s:4:"a02b";s:14:"doc/manual.sxw";s:4:"a139";s:33:"res/templates/codeAsynchronous.js";s:4:"7590";s:32:"res/templates/codeTraditional.js";s:4:"127a";s:16:"static/setup.txt";s:4:"5a95";s:29:"static/asynchronous/setup.txt";s:4:"e921";s:28:"static/general/constants.txt";s:4:"d62e";s:24:"static/general/setup.txt";s:4:"a3aa";s:28:"static/traditional/setup.txt";s:4:"f2dd";}',
 	'suggests' => array(
 	),
 );
